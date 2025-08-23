9d6994f1f924464964ee21d755ef96361bb27f32
Javi Cerezo <jc.webmob@gmail.com>
2025-08-24 00:46:16 +0200

    Agregar componente tecnologías

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Header } from "./components/features/Header/Header";
 import { Portada } from "./components/features/Portada/Portada";
 import { CuadroAmarillo } from "./components/features/CuadroAmarillo/CuadroAmarillo";
-import { Separador } from "./components/features/Separador/Separador";
+import { Tecnologias } from "./components/features/Separador/Tecnologías";
 
 export const App = () => {
   return (
@@ -9,7 +9,7 @@ export const App = () => {
       <Header />
       <Portada />
       <CuadroAmarillo />
-      <Separador />
+      <Tecnologias />
     </>
   );
 };
