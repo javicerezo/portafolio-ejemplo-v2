299468a84d0bab31c541eb0a3f8431a01ad97ce3
Javi Cerezo <jc.webmob@gmail.com>
2025-08-24 13:41:54 +0200

    Maquetar bien componente Header

--- src/components/features/Header/Header.jsx
+++ src/components/features/Header/Header.jsx
@@ -1,44 +1,48 @@
+import { useEffect, useState } from "react";
+
+import { FaSun, FaMoon, FaBars, FaXmark } from "react-icons/fa6";
+import { SiLinkedin, SiGithub  } from "react-icons/si";
 import './Header.scss';
 
 export const Header = () => {
-    const handleDarKMode = () => {
-        const body = document.querySelector('.js-body');
-        body.classList.toggle('darkmode');
-    }
-    const handleBurgerBar = () => {
-        const nav = document.querySelector('.Header-nav');
-        const iconBurger = document.querySelector('.js-header-burger i');
-        nav.classList.toggle('c-header-nav--mod');
-        iconBurger.classList.toggle('fa-bars');
-        iconBurger.classList.toggle('fa-xmark');
-    }
+    const [ posScrollY, setPosScrollY ] = useState(false);
+    const [ darkModeOn, setDarkModeOn ] = useState(false);
+    const [ menuOn, setMenuOn ] = useState(false);
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY === 0) {
+            setPosScrollY(false);  
+        } else {
+            setPosScrollY(true);
+        }
+    });
+
+    useEffect( () => {
+        const body = document.body;
+        body.classList.toggle('darkMode');
+    }, [darkModeOn]);
 
     return (
-        <header className="Header o-container js-header">
+        <header className={`Header ${posScrollY ? "Header--mod" : ""}`}>
             <div className="Header-izquierda">
-                <div className="Header-dark-mode js-header-dark-mode" onClick={handleDarKMode}>
-                    <div className="Header-img">
-                        <img src="public/favicon-transp.png" alt="icono modo oscuro"/>
-                    </div>
-                    <span className="Header-span"><i className="fa-solid fa-moon"></i></span>
-                    <span className="Header-span"><i className="fa-solid fa-sun"></i></span>
+                <div className={`Header-darkMode ${darkModeOn ? "Header-darkMode--mod" : ""}`} onClick={ () => (setDarkModeOn(!darkModeOn)) }>
+                    <img className={`Header-img ${darkModeOn ? "Header-img--mod" : ""}`} src="assets/imgs/favicon-transp.png" alt="icono modo oscuro"/>
+                    <span className="Header-span"><FaMoon /></span>
+                    <span className="Header-span"><FaSun /></span>
                 </div>
                 <p className="Header-autor"><a href="https://javicerezo.netlify.app/" target="_blank"> El Portafolio de NombreApellidos</a>
                 </p>
             </div>
-            <div className="Header-burger js-header-burger" >
-                <input type="checkbox" id="check-menu"/>
-                <label className="Header-burger-icono" htmlFor="check-menu">
-                    <i className="fa-solid fa-bars" onClick={handleBurgerBar}></i>
-                </label>
+            <div className="Header-burger" onClick={ () => (setMenuOn(!menuOn)) }>
+                {menuOn ? (<FaXmark  />) : (<FaBars  />)}
             </div>
-            <ul className="Header-nav">
+            <ul className={`Header-nav ${menuOn ? "Header-nav--mod" : ""}`} >
                 <li className="Header-li"><a className="Header-a" href="#SobreMi">SOBRE MÍ</a></li>
                 <li className="Header-li"><a className="Header-a" href="#Trabajos">PORTAFOLIO</a></li>
                 <li className="Header-li"><a className="Header-a" href="#Contacto">CONTACTO</a></li>
                 <div className="Header-li">
-                    <a className="Header-li-iconos" href="https://linkedin.com/in/javicerezoorellana" target="_blanck"><i className="fa-brands fa-linkedin"></i></a>
-                    <a className="Header-li-iconos" href="https://github.com/javicerezo" target="_blanck"><i className="fa-brands fa-github"></i></a>
+                    <a className="Header-li-iconos" href="https://linkedin.com/in/javicerezoorellana" target="_blanck"><SiLinkedin /></a>
+                    <a className="Header-li-iconos" href="https://github.com/javicerezo" target="_blanck"><SiGithub /></a>
                 </div>
             </ul>
         </header>
