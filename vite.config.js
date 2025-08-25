5254a5dd850585983363fe4835c9bc877b7024c5
Javi Cerezo <jc.webmob@gmail.com>
2025-08-25 10:50:40 +0200

    Cambios en el vite.config.js para subir a producción

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portafolio-ejemplo-react/',   // cambiamos la referencia base para subir a producción
+  build: { outDir: 'docs' }             // Hacemos que npm build genere la carpeta 'dist' dentro 'portafolio-ejemplo-react/docs' para subirla a producción
 })
