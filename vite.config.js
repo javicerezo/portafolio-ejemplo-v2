9beb435cee3a08aa264bc1495176f521f12cd308
Javi Cerezo <jc.webmob@gmail.com>
2025-08-25 15:00:51 +0200

    Cambiar vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'node:url';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portafolio-ejemplo-react/',   // cambiamos la referencia base para subir a producción
+  base: '/portafolio-ejemplo-v2/',   // cambiamos la referencia base para subir a producción
   build: { outDir: 'docs' },            // Hacemos que npm build genere la carpeta 'dist' dentro 'portafolio-ejemplo-react/docs' para subirla a producción
   resolve: {
     alias: {
