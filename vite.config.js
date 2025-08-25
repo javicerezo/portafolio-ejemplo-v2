91290fdc902f0bc5c7a28456854b8cebc315efe1
Javi Cerezo <jc.webmob@gmail.com>
2025-08-25 12:16:08 +0200

    Añadir alias a la configuración de vite.config.js para correcta visualización de imágenes en producción

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/portafolio-ejemplo-react/',   // cambiamos la referencia base para subir a producción
-  build: { outDir: 'docs' }             // Hacemos que npm build genere la carpeta 'dist' dentro 'portafolio-ejemplo-react/docs' para subirla a producción
-})
+  build: { outDir: 'docs' },            // Hacemos que npm build genere la carpeta 'dist' dentro 'portafolio-ejemplo-react/docs' para subirla a producción
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // ← alias a src
+    },
+  },
+});
